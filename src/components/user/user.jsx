de21f144d676295858a93c381ac372bff540227d
Szymon Oponczewski <szymon_o@mac.com>
2023-06-08 22:40:24 +0200

    user.jsx propTypes update

--- src/components/user/user.jsx
+++ src/components/user/user.jsx
@@ -36,9 +36,9 @@ export const Profile = ({
 };
 
 Profile.propTypes = {
-  username: PropTypes.string,
-  tag: PropTypes.string,
-  location: PropTypes.string,
-  avatar: PropTypes.string,
-  stats: PropTypes.objectOf(PropTypes.number),
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.objectOf(PropTypes.number).isRequired,
 };
